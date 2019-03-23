2dbe7b1f28fb8670100837c4c8f5bbd96071e7db
Thomas Wood <thomas.d.wood@gmail.com>
2019-03-23 01:17:26 -0400

    adds user auth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_23_040956) do
+ActiveRecord::Schema.define(version: 2019_03_23_043454) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -45,6 +45,16 @@ ActiveRecord::Schema.define(version: 2019_03_23_040956) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username", null: false
+    t.string "email", null: false
+    t.string "crypted_password", null: false
+    t.string "salt", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
